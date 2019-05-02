678f4b231cf5618f13d40a58696cd2be7dfea4cb
xondtx <ondrejtelka@gmail.com>
2019-05-02 23:50:54 +0200

    Create an arguments parser

--- dpp.py
+++ dpp.py
@@ -1,7 +1,24 @@
 #!venv/bin/python
-import sys
+import argparse
+
 from core.web import DPP
 
-dpp = DPP()
 
-title, connections = dpp.query_connection(sys.argv[1], sys.argv[2])
+parser = argparse.ArgumentParser(description='Process some integers.')
+
+parser.add_argument("start", type=str,
+                    help="the starting station")
+parser.add_argument("via", type=str, nargs='?', default="",
+                    help='via (optional)')
+parser.add_argument("end", type=str,
+                    help='the final station')
+parser.add_argument("-n", type=int, default=3, metavar="NUMBER",
+                    help='number of connections for search')
+
+args = parser.parse_args()
+
+# ------------------------------------------------------------------------------ #
+
+
+dpp = DPP()
+title, connections = dpp.query_connection(src=args.start, dst=args.end, via=args.via, num=args.n)
