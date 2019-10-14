3c132bc34607672ee9c8767baf4d1e86707c577e
xondtx <ondrejtelka@gmail.com>
2019-10-15 01:08:04 +0200

    Prepare the http server

---
+++ server.py
@@ -0,0 +1,28 @@
+import eventlet.wsgi
+import hug.api
+from hug.middleware import CORSMiddleware
+
+
+hug_api = hug.API(__name__)
+hug_api.http.add_middleware(CORSMiddleware(hug_api))  # disable CORS
+
+
+
+@hug.get("/")
+def index():
+	return "some html"
+
+
+
+@hug.get("/api/something")
+def something():
+	return {
+		"some": "json",
+	}
+
+
+
+# start the server
+# print(f"starting dpp server on {env.socketio_host}:{env.socketio_port}") # todo: argparse
+eventlet.wsgi.server(eventlet.listen(addr=("localhost", 8001)), hug_api.http.server(), log_output=True)
+# eventlet.wsgi.server(eventlet.listen(addr=("localhost", 8001)), __hug_wsgi__, log_output=True)
